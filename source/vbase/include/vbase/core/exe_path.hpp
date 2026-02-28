caca8e0e30f5a9843b096a36be522d07f0ac2461
Lazy_V <954294627@qq.com>
2026-02-28 17:52:32 +0000

    feat: add exe path support.

---
+++ source/vbase/include/vbase/core/exe_path.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <filesystem>
+
+#ifdef _WIN32
+#include <windows.h>
+#elif defined(__APPLE__)
+#include <mach-o/dyld.h>
+#else
+#include <unistd.h>
+#endif
+
+namespace vbase
+{
+    std::filesystem::path executable_path()
+    {
+#ifdef _WIN32
+        wchar_t buffer[MAX_PATH];
+        DWORD   len = GetModuleFileNameW(nullptr, buffer, MAX_PATH);
+        return std::filesystem::path(buffer, buffer + len);
+#elif defined(__APPLE__)
+        uint32_t size = 0;
+        _NSGetExecutablePath(nullptr, &size);
+        std::string buffer;
+        buffer.resize(size);
+        _NSGetExecutablePath(buffer.data(), &size);
+        return std::filesystem::weakly_canonical(buffer);
+#else
+        char    buffer[4096];
+        ssize_t len = readlink("/proc/self/exe", buffer, sizeof(buffer));
+        return std::filesystem::path(buffer, buffer + len);
+#endif
+    }
+
+    std::filesystem::path executable_dir() { return executable_path().parent_path(); }
+} // namespace vbase
